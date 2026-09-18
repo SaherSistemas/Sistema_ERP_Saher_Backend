05f9ab4ff61eddaa3829732137e170b895191018
SaherSistemas <sahersistemas@gmail.com>
2026-09-18 13:07:23 -0700

    AGRANDAR MAS PETICIONES

--- src/config/limiter.ts
+++ src/config/limiter.ts
@@ -1,7 +1,7 @@
 import { rateLimit } from 'express-rate-limit';
 
 const generalLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000,
+    windowMs: 5 * 60 * 1000,
     max: 2000,
     keyGenerator: (req) => {
         const auth = req.headers.authorization;
